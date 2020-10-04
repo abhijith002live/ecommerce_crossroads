176d17c79eb74beae3332db7b0b49b627b24aafd
abhijith002live <47815964+abhijith002live@users.noreply.github.com>
2020-10-04 12:21:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+var createError = require('http-errors');//
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -13,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');//
 app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout',partialsDir:__dirname+'/views/partials'}))
 app.use(logger('dev'));
 app.use(express.json());
